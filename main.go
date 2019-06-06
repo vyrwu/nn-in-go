71d9ffd05554dc6f7ff4a57ca052f3dff4b31cd0
vyrwu <alek_nowak@hotmail.com>
2019-06-06 18:34:19 +0200

    Clean-up

--- main.go
+++ main.go
@@ -1,10 +1,4 @@
 package main
 
-// Preprocess
-// Compare to dfferent architectures
-// show true pos neg etc.
-// explain why one arch was better than the other
-// dimentionality reduction
-// etc
 func main() {}
 
